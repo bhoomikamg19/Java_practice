e6134d9c3e61c618befcefdd6b4cd900e376ee8d
bhoomikamg19 <bhoomikamg19@gmail.com>
2025-05-23 16:55:57 +0530

    Array

--- sample/src/Ar6.java
+++ sample/src/Ar6.java
@@ -1,5 +1,9 @@
 public class Ar6 {
     public static void main(String[] args) {
+        String[] mobiles={"oppo","vivo","apple","samsung","realme","redmi","iqoo","moto","oneplus","nokia","google"};
+        for(String mobile:mobiles){
+            System.out.println(mobile);
+        }
 
     }
 }
