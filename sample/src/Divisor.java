6884feaeb027520609e04db8b162f9e17f504a6c
bhoomikamg19 <bhoomikamg19@gmail.com>
2025-05-24 00:43:05 +0530

    divisor

---
+++ sample/src/Divisor.java
@@ -0,0 +1,10 @@
+public class Divisor {
+    public static void main(String[] args) {
+        int num=20;
+        for(int i=1; i<=20;i++){
+            if(num % i == 0){
+                System.out.println("Divisor of 20: " + i);
+            }
+        }
+    }
+}
