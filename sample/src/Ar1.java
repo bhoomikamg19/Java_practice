e90946ada3840057bf2d26b4d79cbf403b7bffa3
bhoomikamg19 <bhoomikamg19@gmail.com>
2025-05-23 16:41:16 +0530

    Array

--- sample/src/Ar1.java
+++ sample/src/Ar1.java
@@ -1,9 +1,8 @@
 public class Ar1 {
     public static void main(String[] args) {
         String[] names={"Nidhi","diya","akash","namrata","spoo","aryan","ananya","ganji","shashank","yashu"};
-        for(int i=1;i<names.length;i++){
-            System.out.println(names[i]);
-        }
+        for(String name:names){
+            System.out.println(name);        }
 
     }
 }
---
+++ sample/src/Ar2.java
@@ -0,0 +1,9 @@
+public class Ar2 {
+    public static void main(String[] args) {
+        String[] movies={"sultan","arya","kgf","abhi","ajay","milana","pruthvi","daasa","RRR","lovemoctail"};
+        for(String movie:movies){
+            System.out.println(movie);
+        }
+
+    }
+}
