624415d7c171802fa6ed265f389d48ea84fd069b
bhoomikamg19 <bhoomikamg19@gmail.com>
2025-05-24 00:39:48 +0530

    table

---
+++ sample/src/table1.java
@@ -0,0 +1,11 @@
+public class table1 {
+    public static void main(String[] args) {
+        int num=2;
+        for(int i=1;i<=10;i++ ){
+            if(i % 2 == 0){
+                System.out.println("2 * " + i + "= " + (num*i));
+            }
+        }
+
+    }
+}
