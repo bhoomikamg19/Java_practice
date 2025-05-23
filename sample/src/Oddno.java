2c3537b094fc2f45435e7fb63bd2e65d4b17c7a5
bhoomikamg19 <bhoomikamg19@gmail.com>
2025-05-23 23:49:02 +0530

    odd no

---
+++ sample/src/Oddno.java
@@ -0,0 +1,10 @@
+public class Oddno {
+    public static void main(String[] args) {
+        for(int i=20;i<=30;i++ ){
+            if(i % 2 != 0){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
