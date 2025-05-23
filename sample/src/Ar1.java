12d816aad01cb513125c6a9249396204bf65ef51
bhoomikamg19 <bhoomikamg19@gmail.com>
2025-05-23 16:36:41 +0530

    Array

---
+++ sample/src/Ar1.java
@@ -0,0 +1,9 @@
+public class Ar1 {
+    public static void main(String[] args) {
+        String[] names={"Nidhi","diya","akash","namrata","spoo","aryan","ananya","ganji","shashank","yashu"};
+        for(int i=1;i<names.length;i++){
+            System.out.println(names[i]);
+        }
+
+    }
+}
